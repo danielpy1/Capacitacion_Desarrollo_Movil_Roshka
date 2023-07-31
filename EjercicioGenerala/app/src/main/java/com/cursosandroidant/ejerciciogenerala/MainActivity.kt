a28d9e2444eff27d834a03c06436d93a5f580094
Daniel Dos Santos <cdosantos@roshka.com>
2023-07-31 17:15:24 -0400

    Seteo dinamico de dados

--- EjercicioGenerala/app/src/main/java/com/cursosandroidant/ejerciciogenerala/MainActivity.kt
+++ EjercicioGenerala/app/src/main/java/com/cursosandroidant/ejerciciogenerala/MainActivity.kt
@@ -2,10 +2,28 @@ package com.cursosandroidant.ejerciciogenerala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnJugar: Button = findViewById(R.id.btnPlay)
+        btnJugar.setOnClickListener {
+        var dados:MutableList<Int> = mutableListOf()
+        var imagenesDados:MutableList<ImageView> = mutableListOf()
+            for(i in 1..5){
+                dados.add(Random.nextInt(1,7))
+                val resID = resources.getIdentifier("dice$i", "id", packageName)
+                val imageView = findViewById<ImageView>(resID)
+                imagenesDados.add(imageView)
+                val resourceID = resources.getIdentifier("d${dados[i-1]}", "drawable", packageName)
+                imagenesDados[i-1].setImageResource(resourceID)
+            }
+        }
     }
 }
